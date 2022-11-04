f2e6a9d4d3711271d48f0d8c81424abd476111c9
azat valiev <azvaliev@gmail.com>
2022-11-03 20:08:46 -0500

    improved navbar responsiveness

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -11,7 +11,7 @@ const navitems = [{
 
 function Navbar(): JSX.Element {
   return (
-    <nav className="flex w-full h-[10vh] bg-[hsla(0,0%,100%,.88)] items-center justify-end gap-4 md:gap-8 px-4 z-20 sticky top-0">
+    <nav className="flex w-full h-[10vh] bg-[hsla(0,0%,100%,.88)] items-center justify-around sm:justify-end sm:gap-4 md:gap-8 px-4 z-20 sticky top-0">
       {navitems.map((item) => (
         <a href={item.link} key={item.link} className="text-black text-2xl font-[everettthin] h-fit">
           {item.text}
