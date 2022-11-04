5358f15529a8bda9b5db0717f2e40fc4442b6e19
azat valiev <azvaliev@gmail.com>
2022-11-03 22:22:29 -0500

    perfect timeline

--- src/components/timeline/index.tsx
+++ src/components/timeline/index.tsx
@@ -4,31 +4,37 @@ import timelinedata from './data.json';
 
 function Timeline(): JSX.Element {
   return (
-    <section className="min-h-screen py-8">
-      <h2 className="font-[everettthin] text-5xl uppercase text-center">
+    <section className="flex flex-col w-full min-h-screen py-8">
+      <h2 className="font-[everettthin] text-5xl uppercase text-center mb-8">
         Experience
       </h2>
-      <div className="timeline flex flex-col">
+      <ol className="mx-auto w-[95%] md:max-w-[40%] relative border-l border-gray-200 dark:border-gray-700">
         {timelinedata.map((point) => (
-          <div
-            className="bg-gray-50 p-2 px-4 shadow-2xl w-[22rem] text-center mb-6 rounded-xl"
-            key={point.title}
-          >
-            <h3 className="text-2xl underline font-[everettmedium]">
+          <li className="mb-10 ml-4" key={point.title}>
+            <div
+              className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+             />
+            <time
+              className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+            >
+              {point.date}{point.enddate && ` - ${point.enddate}`}
+            </time>
+            <h3
+              className="text-2xl font-semibold text-gray-900"
+            >
               {point.title}
             </h3>
-            <p className="my-3">
+            <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
               {point.desc}
             </p>
             <a
               href={point.cta.link}
-              rel="noopener noreferrer"
-              target="_blank"
-              className="btn block w-fit mt-4"
+              className="inline-flex items-center py-2 px-4 font-[everettlight] border-[1px] border-black
+              transition-colors rounded-md hover:bg-black hover:text-white"
             >
               {point.cta.text}
             </a>
-            <div className="flex flex-wrap justify-around w-3/4 sm:w-2/3 md:w-2/3 mx-auto my-4">
+            <div className="flex flex-wrap ml-0 flex-start gap-2 w-[60%] md:w-2/3 lg:w-1/2 xl:w-[40%] mx-auto my-4">
               {point.icons.map((icon, idx) => (
                 <img
                   src={`/img/techicons/${icon}.webp`}
@@ -39,9 +45,9 @@ function Timeline(): JSX.Element {
                 />
               ))}
             </div>
-          </div>
+          </li>
         ))}
-      </div>
+      </ol>
     </section>
   )
 }
