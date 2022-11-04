1ade4b4eab9161c19e30fb3e821325621d63b319
azat valiev <azvaliev@gmail.com>
2022-11-04 09:29:03 -0500

    Better contrast timeline timestamps

--- src/components/timeline/index.tsx
+++ src/components/timeline/index.tsx
@@ -13,7 +13,7 @@ function Timeline(): JSX.Element {
               className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
              />
             <time
-              className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+              className="mb-1 text-sm font-normal leading-none text-gray-600"
             >
               {point.date}{point.enddate && ` - ${point.enddate}`}
             </time>
