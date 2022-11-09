79abf1237fb6143dfbd8fc18c47c183bb6891908
azat valiev <azvaliev@gmail.com>
2022-11-08 19:24:01 -0600

    Fully responsive portfolio page

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -19,7 +19,7 @@ function Portfolio(): JSX.Element {
   return (
     <>
       <Navbar items={navitems} />
-      <h1 className="text-5xl text-center font-medium font-everett-regular">
+      <h1 className="text-5xl text-center font-medium font-everett-regular underline mt-4 -mb-4 md:mb-0">
         Portfolio
       </h1>
       <main>
@@ -27,11 +27,15 @@ function Portfolio(): JSX.Element {
           <div
             key={portfolioItem.title}
             className={
-              `flex items-center justify-around mt-14 rounded-sm
-               ${idx % 2 ? 'flex-row-reverse' : 'flex-row'}`
+              `flex items-center justify-around mt-14 rounded-sm flex-col
+               ${idx % 2 ? 'lg:flex-row-reverse' : 'lg:flex-row'}`
              }
           >
-            <div className="flex flex-col items-center w-[40%] text-center py-2 px-4 shadow-xl">
+            <div
+            className={`
+              flex flex-col items-center w-[90%] md:w-[70%] lg:w-[40%] text-center
+              py-2 px-4 shadow-xl mb-8 lg:mb-0
+            `}>
               <h2 className="text-3xl font-everett-regular">
                 {portfolioItem.title}
               </h2>
@@ -41,7 +45,7 @@ function Portfolio(): JSX.Element {
               </p>
               <div className="flex gap-2 my-6">
                 {portfolioItem.links.map(({ link, name }, idx) => (
-                  <a 
+                  <a
                     key={link}
                     href={link}
                     className={
@@ -60,7 +64,11 @@ function Portfolio(): JSX.Element {
               <h3 className="text-2xl font-everett-regular mb-3 underline">
                 Technologies Used
               </h3>
-              <div className="flex flex-row flex-wrap gap-2 mb-4 max-w-[90%] mx-auto justify-between">
+              <div
+                className={`
+                flex flex-row flex-wrap gap-2 mb-4 max-w-[65%]
+                md:max-w-[80%] lg:max-w-[90%] mx-auto justify-between
+                `}>
                 {portfolioItem.tech.map((tech) => (
                   <img
                     key={`${portfolioItem.title}-${portfolioItem.tech}`}
@@ -69,11 +77,10 @@ function Portfolio(): JSX.Element {
                     src={titleToIconsMapping[tech]}
                     className="m-1 h-[8.75vw] w-[8.75vw] sm:w-[6vw] sm:h-[6vw] md:h-[4.5vw] md:w-[4.5vw] lg:h-[2.75vw] lg:w-[2.75vw]"
                   />
-                    
-                ))} 
+                ))}
               </div>
             </div>
-            <div className="w-1/2">
+            <div className="w-5/6 lg:w-1/2">
               <img
                 src={portfolioItem.img}
                 className="w-full h-full object-contain"
