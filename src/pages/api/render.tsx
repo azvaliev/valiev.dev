e5f8a8283cc9c2d8d8bee81a0ea6322b6fc22a1e
Azat Valiev <azat@valievventures.com>
2024-02-10 11:10:50 -0600

    fix wacky build step

--- src/pages/api/render.tsx
+++ src/pages/api/render.tsx
@@ -3,7 +3,6 @@ import { execSync } from 'child_process';
 
 import Root from '../../root';
 import Home from '..';
-import Portfolio from '../portfolio'
 
 import ReactDOMServer from 'react-dom/server';
 import type { NextApiRequest, NextApiResponse } from 'next';
@@ -15,12 +14,6 @@ async function handler(_req: NextApiRequest, res: NextApiResponse) {
     </Root>
   )
 
-  const portfolioHTML = ReactDOMServer.renderToStaticMarkup(
-    <Root>
-      <Portfolio />
-    </Root>
-  )
-
   if (!fs.existsSync('dist')) {
     console.info('making dist...');
     execSync('mkdir dist');
@@ -28,7 +21,6 @@ async function handler(_req: NextApiRequest, res: NextApiResponse) {
 
   console.info('writing html');
   fs.writeFileSync('dist/index.html', `<!DOCTYPE html>${indexHTML}`);
-  fs.writeFileSync('dist/portfolio.html', `<!DOCTYPE html>${portfolioHTML}`);
 
   console.info('copying public');
   execSync('cp -R public/* dist/');
